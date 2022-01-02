06cf4b9fa02ebced3d5602e57c42a7ffa448434c
Tuan Ma <tuan.ma@me.com>
2022-01-01 21:59:10 -0800

    Update Logo

--- js/packages/web/src/components/AppBar/index.tsx
+++ js/packages/web/src/components/AppBar/index.tsx
@@ -51,7 +51,7 @@ export const MetaplexMenu = () => {
     return (
       <>
         <Modal
-          title={<img src={'/playersonly-logo.svg'} />}
+          title={<img src={'/PO-logo.svg'} />}
           visible={isModalVisible}
           footer={null}
           className={'modal-box'}
@@ -110,7 +110,7 @@ export const MetaplexMenu = () => {
 export const LogoLink = () => {
   return (
     <Link to={`/`}>
-      <img src={'/playersonly-logo.svg'} />
+      <img src={'/PO-logo.svg'} />
     </Link>
   );
 };
